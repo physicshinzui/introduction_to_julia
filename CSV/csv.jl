246edeaa2b34ca40f29a45830e141e0aaa82da8c
Shinji Iida <physicshinzui@gmail.com>
2021-07-13 21:12:57 +0900

    dataframe manipulation notebook was added.

--- CSV/csv.jl
+++ CSV/csv.jl
@@ -5,7 +5,7 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 30d536ac-e30a-11eb-31c0-0d58eff09d6f
-using CSV, DataFrames, PlutoUI
+using CSV, DataFrames, PlutoUI, DataFramesMeta, HTTP, Plots
 
 # ╔═╡ c8981ac6-8bbe-4cb9-a555-251240345888
 md"""
@@ -17,7 +17,7 @@ Written by Shinji Iida
 VERSION
 
 # ╔═╡ 2f629cc9-f391-4c71-89f3-76cc4b40e8ae
-csv_reader = CSV.File("file.csv")
+csv_reader = CSV.File("dataset/file.csv")
 
 # ╔═╡ 483b0b82-d2b4-43d7-b9f6-14de2d89b22c
 typeof(csv_reader)
@@ -27,12 +27,12 @@ md"## CSV.File('file.csv')でCSV objectを作成して，DataFrame()に食わせ
 
 # ╔═╡ 77c79828-c6b3-432c-b8f3-d93960902503
 begin
-csv_obj = CSV.File("file.csv")
+csv_obj = CSV.File("dataset/file.csv")
 #typeof(csv_obj)
 end
 
 # ╔═╡ 37fa15f3-9571-4e1d-be78-43b97f23a113
-df1 = DataFrame(CSV.File("file.csv"))
+df1 = DataFrame(CSV.File("dataset/file.csv"))
 
 # ╔═╡ 52b7c15e-8a43-4a7a-abe6-bb9b9ba2b1a5
 # Iterate csv_reader: rows are given
@@ -44,17 +44,38 @@ end
 
 # ╔═╡ fc578535-b163-41de-bf6d-ce451aa4bc7d
 ## Pipe
-df2 = CSV.File("file.csv") |> DataFrame
+df2 = CSV.File("dataset/file.csv") |> DataFrame
 
 # ╔═╡ 644e9bf2-226f-45d0-92ad-939f53dd84fe
 ## CSV.read()
-df3 = CSV.read("file.csv", DataFrame)
+df3 = CSV.read("dataset/file.csv", DataFrame)
 
 # ╔═╡ 15023596-163b-4c0a-a709-63ad65b0833b
-
+md"""
+## 特定の行だけを抽出する
+使うパッケージ
+- DataFrameMeta.jl
+- HTTP.jl
+まずはデータを読み込み，dataframeを作ると以下のようになります．
+"""
 
 # ╔═╡ b2268c84-34af-4172-81ec-447c3f85552d
+begin 
+	url = "http://marid.bioc.cam.ac.uk/sdm2/static/datasets/dataset_S2648.csv"
+	df = DataFrame(CSV.File(HTTP.get(url).body)) # HTTP.get().body is from HTTP.jl 
+end
 
+# ╔═╡ 7539c44f-fb93-4a56-8fb6-55a4832c8f77
+@linq df_ph_filtered = df |> where(:PH .>= 6.5, :PH .<= 7.5) |> groupby(:MUTANT_RES)
+
+# ╔═╡ 8a0196ea-0165-433f-8b3d-738c0fd1124c
+df_ph_filtered.keymap
+
+# ╔═╡ e4c68e1f-d9bd-4a1a-bf35-f705ef9af2e5
+df_ph_filtered[1]
+
+# ╔═╡ f2b041ea-4b48-429f-ac7e-f674882c00f0
+histogram(df_ph_filtered[9].EXP_DDG, bins=50)
 
 # ╔═╡ Cell order:
 # ╠═c8981ac6-8bbe-4cb9-a555-251240345888
@@ -70,3 +91,7 @@ df3 = CSV.read("file.csv", DataFrame)
 # ╠═644e9bf2-226f-45d0-92ad-939f53dd84fe
 # ╠═15023596-163b-4c0a-a709-63ad65b0833b
 # ╠═b2268c84-34af-4172-81ec-447c3f85552d
+# ╠═7539c44f-fb93-4a56-8fb6-55a4832c8f77
+# ╠═8a0196ea-0165-433f-8b3d-738c0fd1124c
+# ╠═e4c68e1f-d9bd-4a1a-bf35-f705ef9af2e5
+# ╠═f2b041ea-4b48-429f-ac7e-f674882c00f0
--- ann/ann.jl
+++ ann/ann.jl
@@ -21,6 +21,13 @@ begin
 	function relu(x) 
 		return max(0, x)
 	end
+	function softmax(xs)
+		y =  exp.(xs)
+		return y ./ sum(y)
+	end
+	function identity(x)
+		return x
+	end
 #	function softmax(x)
 #		return exp(x) /exp()
 #	end
@@ -32,16 +39,26 @@ begin
 	ystep = step.(x)
 	ys = sigmoid.(x)
 	yr = relu.(x)
-	plot(x, ystep, label="Step") # 
+	ysoft = softmax(x) 
+	yi = identity.(x)
+	plot(x, ystep, label="Step", ylims=[-0.5,2.0])  
 	plot!(x, ys, label="Sigmoid")
-	plot!(x,yr, label="reLu")
-	plot!()
+	plot!(x,yr, label="ReLu", lw =3)
+	plot!(x,ysoft, label="Softmax", lw =3)	
+	plot!(x, yi, label="Identity", color=:black)
 end
+
+# ╔═╡ 15a22932-d85e-4f4f-9e23-ff0de5b4642d
+sum(softmax.(x))
 
 # ╔═╡ fae14f3f-ead2-4382-ab37-0af29419ac40
 md"""# Neutral network
 
-$\boldsymbol{A} = \boldsymbol{x^T}\boldsymbol{W} + \boldsymbol{b}$
+$\boldsymbol{A}^T = \boldsymbol{x}^T\boldsymbol{W}^T + \boldsymbol{b}^T$
+(Row vector representation)
+
+$\boldsymbol{A} = \boldsymbol{W}\boldsymbol{x} + \boldsymbol{b}$ 
+(This is colum-vector representation )
 
 b: control the extent of conbustion.
 
@@ -50,12 +67,12 @@ b: control the extent of conbustion.
 # ╔═╡ 141197b6-44b3-4fca-8d94-92827a04783c
 function init_network()
 	network = Dict()
-	network["b1"] = [0.1 0.1 0.1 ;]
-	network["b2"] = [0.1 0.1 0.1 ;]
-	network["b3"] = [0.1 0.1 0.1 ;]
-	network["W1"] = [0.1 0.1 0.1 ; 0.1 0.1 0.1] 
-	network["W2"] = [0.1 0.1 0.1 ; 0.1 0.1 0.1] 
-	network["W3"] = [0.1 0.1 0.1 ; 0.1  0.1 0.1] 
+	network["b1"] = [0.1, 0.1, 0.1]
+	network["b2"] = [0.1, 0.1, 0.1]
+	network["b3"] = [0.1, 0.1, 0.1]
+	network["W1"] = [0.1 0.1 ; 0.1 0.1 ; 0.1 0.1] 
+	network["W2"] = [0.1 0.1 ; 0.1 0.1 ; 0.1 0.1] 
+	network["W3"] = [0.1 0.1 ; 0.1 0.1 ; 0.1 0.1] 
 	
 	return network
 end
@@ -65,12 +82,14 @@ function forward(network, x)
 	W1, W2, W3 = network["W1"], network["W2"], network["W3"]
 	b1, b2, b3 = network["b1"], network["b2"], network["b3"]
 	
-	a1 = x*W1 + b1  	
+	a1 = W1*x + b1  	
 	z1 = sigmoid.(a1)
-#	a2 = z1*W2 + b2
-#	z2 = sigmoid.(a2)
-#	a3 = x2*W3 + b3
-#	z3 = sigmoid.(a3)
+	
+	a2 = W2*z1 + b2
+	z2 = sigmoid.(a2)
+	
+	a3 = W3*z2 + b3
+	z3 = sigmoid.(a3)
 end
 
 # ╔═╡ 3647c957-25a1-4091-9771-6765fd15c842
@@ -78,7 +97,7 @@ model = init_network()
 
 # ╔═╡ d950cece-bee3-4cd7-9fdb-92e951a95510
 begin
-	features = [1.0 0.5;]
+	features = [1.0, 0.5]
 	forward(model, features)
 end
 
@@ -87,6 +106,7 @@ end
 # ╠═19e38f57-52e5-4bbe-879e-2363b0c2972a
 # ╠═b1964bf1-0b81-4a75-896d-1c6e7c0cd2f3
 # ╠═94616701-9c11-4c3c-ab2e-d5b0b258ddd7
+# ╠═15a22932-d85e-4f4f-9e23-ff0de5b4642d
 # ╠═fae14f3f-ead2-4382-ab37-0af29419ac40
 # ╠═141197b6-44b3-4fca-8d94-92827a04783c
 # ╠═ec6435b3-43d0-45b6-b497-280c4756b6ee
